609bd036b7311349ba39a92a9d385cacea82b9a8
IqbalRadjasa <iqbal.radjasa@gmail.com>
2025-12-02 08:33:01 +0700

    API Port Management

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"api-lms-dev/controllers"
 	"api-lms-dev/database"
 	"api-lms-dev/models"
 
+	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 	r := gin.Default()
 
 	database.Connect()
@@ -20,5 +25,13 @@ func main(){
 	r.PUT("/users/:id", controllers.UpdateUser)
 	r.DELETE("/users/:id", controllers.DeleteUser)
 
-	r.Run(":8085")
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiPort := os.Getenv("API_PORT")
+
+	r.Run(":" + apiPort)
 }
