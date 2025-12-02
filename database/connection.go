05f45e593547ccd980cc29969de3bbb417c51df3
IqbalRadjasa <iqbal.radjasa@gmail.com>
2025-12-02 08:24:25 +0700

    Adding .gitignore, godotenv package, and change connection

--- database/connection.go
+++ database/connection.go
@@ -3,7 +3,9 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,7 +13,21 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "root:@tcp(127.0.0.1:3306)/lms_dev?charset=utf8mb4&parseTime=True&loc=Local"
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
